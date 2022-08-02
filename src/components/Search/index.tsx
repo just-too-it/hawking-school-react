7808da2eabc65c28b0e7e6d22506ff142143462b
Natasha <just.too.it@gmail.com>
2022-08-02 19:25:15 +0300

    добавлена запись и считывание url для поиска новостей

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import styles from './Search.module.scss';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
 import { ReactComponent as SearchIcon } from '../../assets/images/icons/search.svg';
 import { Formik, Form } from 'formik';
 import { SButtonIcon } from '../UI/SButtonIcon';
@@ -9,10 +10,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { newsSlice } from '../../store/news/news.slice';
 import { SearchWrapper } from '../SearchWrapper';
 
+import styles from './Search.module.scss';
+
 export const Search = () => {
   const { query } = useSelector((state: RootState) => state.newsReducer);
   const { setQuery } = newsSlice.actions;
   const dispatch = useDispatch();
+  const [queryParams, setQueryParams] = useSearchParams();
+  const newsQuery = queryParams.get('query');
+
+  useEffect(() => {
+    newsQuery && dispatch(setQuery(newsQuery));
+  }, []);
 
   const initialValues = {
     query: query,
@@ -28,23 +37,23 @@ export const Search = () => {
           resetForm();
           setSubmitting(false);
           dispatch(setQuery(values.query));
+          setQueryParams({ query: values.query });
         }, 400);
       }}
     >
       {({ handleSubmit }) => (
         <SearchWrapper>
           <Form>
-          <fieldset className={styles.wrapper}>
-            <SInput type="text" placeholder="Поиск по статьям" name={'query'} className={styles.input} />
-            <div className={styles.button}>
-              <SButtonIcon type="submit" btnOnClick={handleSubmit}>
-                <SearchIcon width={'17.07'} height={'17.07'} />
-              </SButtonIcon>
-            </div>
-          </fieldset>
-        </Form>
+            <fieldset className={styles.wrapper}>
+              <SInput type="text" placeholder="Поиск по статьям" name={'query'} className={styles.input} />
+              <div className={styles.button}>
+                <SButtonIcon type="submit" btnOnClick={handleSubmit}>
+                  <SearchIcon width={'17.07'} height={'17.07'} />
+                </SButtonIcon>
+              </div>
+            </fieldset>
+          </Form>
         </SearchWrapper>
-        
       )}
     </Formik>
   );
